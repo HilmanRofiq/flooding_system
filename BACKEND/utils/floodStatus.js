39ff24543e21a67c5aa59ed115aa2166fae317d7
HilmanRofiq <dzakwanurrofiqhilman@gmail.com>
2026-04-16 12:19:33 +0700

    fix more bug

--- BACKEND/utils/floodStatus.js
+++ BACKEND/utils/floodStatus.js
@@ -1,7 +1,7 @@
 function getFloodStatus(tinggi_air) {
-  if (tinggi_air < 270) return "AMAN";
+  if (tinggi_air < 210) return "AMAN";
   if (tinggi_air < 230) return "WASPADA";
-  if (tinggi_air < 210) return "SIAGA";
+  if (tinggi_air < 270) return "SIAGA";
   return "BAHAYA";
 }
 
